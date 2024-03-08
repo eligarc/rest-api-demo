681e1f3ccdacdbf90058fd9cf3ed2907273fc8c0
CIAEGGAR <elioenai.garcia@proveedoresgnp.mx>
2024-03-08 09:38:16 -0600

    Se agrega configuración inicial del proyecto

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,18 @@
+"use strict";
+const User = use("App/Models/User");
+
+class UserController {
+  async store({ request }) {
+    const { email, password } = request.all();
+    console.log(email, password);
+    const user = await User.create({
+      email,
+      password,
+      username: email,
+    });
+
+    return user;
+  }
+}
+
+module.exports = UserController;
